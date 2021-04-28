b68f3eaf6a9d46ab1a637017f537e839c0df10b7
Vishal Dubey <yzvishal.vd@gmail.com>
2021-04-28 12:34:05 +0530

    added overlay for header

--- src/Components/BreadCrumb.tsx
+++ src/Components/BreadCrumb.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import React from "react";
+import styles from './breadcrumb.module.css';
 
 interface linkType {
 	link: string;
@@ -13,25 +14,25 @@ interface BreadCrumbProps {
 }
 
 const BreadCrumb: React.FC<BreadCrumbProps> = (props) => {
-	const {links, finalName, backgroundImage, title} = props;
+	const { links, finalName, backgroundImage, title } = props;
 
 	return (
-		<div className="breadcrumb-area  pt-50 pb-70 mb-100" style={{backgroundImage: `url(${backgroundImage})`}}>
+		<div className={`breadcrumb-area  pt-50 pb-70 mb-100`} style={{ backgroundImage: `url(${backgroundImage}), linear-gradient(rgba(0,0,0,0.3),rgba(0,0,0,0.3))`, backgroundBlendMode: "overlay" }}>
 			<div className="container">
 				<div className="row">
 					<div className="col-lg-12">
-						<h1 className="breadcrumb-title" style={{color: "white"}}>{title}</h1>
+						<h1 className="breadcrumb-title" style={{ color: "white" }}>{title}</h1>
 						{/*=======  breadcrumb list  =======*/}
 						<ul className="breadcrumb-list">
-							{links.map(({link, name}, index) => (
+							{links.map(({ link, name }, index) => (
 								<li className="breadcrumb-list__item" key={index}>
 									<Link href={link}>
-										<a>{name}</a>
+										<a style={{color: '#f1f1f1'}}>{name}</a>
 									</Link>
 								</li>
 							))}
 
-							<li className="breadcrumb-list__item breadcrumb-list__item--active">{finalName}</li>
+							<li className="breadcrumb-list__item breadcrumb-list__item--active" style={{color: '#F5F5F5'}}>{finalName}</li>
 						</ul>
 						{/*=======  End of breadcrumb list  =======*/}
 					</div>
